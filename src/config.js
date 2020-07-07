37441d85053298ff00b4c16c42ccd271468b305c
Richard Young <richardjy@users.noreply.github.com>
2020-07-06 21:15:08 -0700

    locationIQ

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 // config.js
 var config = { // this should be const instead if you're using ES6 standards
   // this is special key - only for FPE web site
-  mapBoxKey: 'pk.eyJ1IjoicmljaGFyZGp5IiwiYSI6ImNqd2Jtcm43cjAwcTE0YXFyY3RmNnlwMjEifQ.xJi9_3hKL3Q3mj7TZts1Nw'
+  mapBoxKey: 'pk.eyJ1IjoicmljaGFyZGp5IiwiYSI6ImNqd2Jtcm43cjAwcTE0YXFyY3RmNnlwMjEifQ.xJi9_3hKL3Q3mj7TZts1Nw',
+  locIQkey: 'pk.fc8047bdd188b9475af518b800983d3a'
 }
